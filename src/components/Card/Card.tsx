81d7f56b6f552bf040a920591487794f52c724ee
pedrofel1x <fxpedro95@gmail.com>
2026-03-25 22:02:04 +0000

    Coloquei lógica para definir filme/serie

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,20 @@
 import type { IMovie } from "../../types/movie";
 import type { ISerie } from "../../types/Serie";
-import "./card.css";
+import style from "./card.module.css";
 
 interface IMovieCard {
-  content: IMovie;
+  content: IMovie | ISerie;
 }
 function Card({ content }: IMovieCard) {
+  const name =
+    `original_title` in content
+      ? content.original_title
+      : content.original_name;
   return (
-    <div className="card">
-      <h3>{content.original_title}</h3>
+    <div className={style.card}>
       <img src={`https://image.tmdb.org/t/p/w500${content.poster_path}`} />
+      <strong>{name}</strong>
+      <span></span>
     </div>
   );
 }
