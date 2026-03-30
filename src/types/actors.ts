9db3f6456eab4d5b245d628f79df604e9f0f9f33
pedrofel1x <fxpedro95@gmail.com>
2026-03-30 22:15:51 +0100

    actors type created

---
+++ src/types/actors.ts
@@ -0,0 +1,14 @@
+export interface actors {
+    adult: string;
+    gender: number
+    id: number;
+    know_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
+}
