f26ef1621c5b1fd18efa38a4e28f2ce879dd30f9
pedrofel1x <fxpedro95@gmail.com>
2026-04-01 20:01:52 +0100

    Updated routes on navbar for SeriesList

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import Home from "./Home.tsx";
 import MoviesList from "./pages/MoviesList.tsx";
 import NavBar from "./components/NavBar/NavBar.tsx";
 import SeriesList from "./pages/SeriesList.tsx";
+import Card from "./components/Card/Card.tsx";
+import DetailsPage from "./pages/DetailsPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <App /> */}
-    <NavBar />
-    <Home />
-    <MoviesList />
-    <SeriesList />
+    <BrowserRouter>
+      {" "}
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movieslist">
+          <Route index element={<MoviesList />} />
+          <Route path=":filme" element={<DetailsPage />} />
+        </Route>
+        <Route path="serieslist">
+          <Route index element={<SeriesList />} />
+          <Route path=":serie" element={<DetailsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    {/* <MoviesList />
+    <SeriesList /> */}
   </StrictMode>,
 );
