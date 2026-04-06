763006559b183a5a7844b9b50ca6ec209084d83e
pedrofel1x <fxpedro95@gmail.com>
2026-04-06 18:10:58 +0100

    added popular list and upcoming

--- src/home.tsx
+++ src/home.tsx
@@ -1,9 +1,14 @@
 import Botao from "./components/Botao/Botao";
+import PopularList from "./pages/PopularList";
+import UpComingList from "./pages/UpComingList";
 
 function Home() {
   return (
     <>
       <h1>MovieDB EDIT</h1>
+
+      <PopularList />
+      <UpComingList />
     </>
   );
 }
